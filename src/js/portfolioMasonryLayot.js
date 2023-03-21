359413c11e72d861308da3852365fdd3f8f0732b
Lord-Bambucha <lordbambycha93@gmail.com>
2023-03-21 23:05:51 +0200

    1

--- src/js/portfolioMasonryLayot.js
+++ src/js/portfolioMasonryLayot.js
@@ -16,5 +16,7 @@ var msnry = new Masonry(elem, {
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+var lightbox = new SimpleLightbox('.gallery a', {
+    // fadeSpeed: 300,
+});
 
