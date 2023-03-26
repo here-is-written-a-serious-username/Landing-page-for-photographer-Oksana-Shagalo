434a5162d776566b3ff54b604403cecf8ca8bfe4
Lord-Bambucha <lordbambycha93@gmail.com>
2023-03-27 00:24:26 +0300

    1

--- src/js/heroSlider.js
+++ src/js/heroSlider.js
@@ -1,16 +1,20 @@
-const images = ['hero-1.jpg', 'hero-2.jpg', 'hero-3.jpg'];
+const images = ['hero1', 'hero2', 'hero3'];
 let currentIndex = 0;
 const sliderImage = document.querySelector('.hero');
 
 function changeImage() {
-    currentIndex++;
     if (currentIndex >= images.length) {
         currentIndex = 0;
+        sliderImage.classList.remove(images[images.length - 1]);
     }
     const currentImage = images[currentIndex];
-    sliderImage.style.backgroundImage = `url(../img/hero/${currentImage})`;
+    sliderImage.classList.add(images[currentIndex]);
+    sliderImage.classList.remove(images[currentIndex - 1]);
+
+    currentIndex += 1;
+    // sliderImage.style.backgroundImage = `url(${currentImage})`;
     // console.log(sliderImage.style.backgroundImage = `url(../img/hero/${currentImage})`);
 }
 
-setInterval(changeImage, 5000);
+setInterval(changeImage, 15000);
 
